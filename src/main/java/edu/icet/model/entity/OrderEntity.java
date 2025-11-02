8b06be229ed4fbc1501c906f9079c874f8112498
Oshadha <hpgmoshadhapalliyaguru@gmail.com>
2025-11-02 10:14:16 +0530

    Entity Classes done

---
+++ src/main/java/edu/icet/model/entity/OrderEntity.java
@@ -0,0 +1,23 @@
+package edu.icet.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDate;
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+@Entity
+public class OrderEntity {
+    @Id
+    private String id;
+    private LocalDate date ;
+    private String customerId;
+    private String status;
+    private String item_name;
+    private String item_quantity;
+}
